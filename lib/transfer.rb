ef9df308b106fb9bf8435e4eeea186735d49beed
Seth Todd <seth.todd@outlook.com>
2020-03-21 16:02:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,12 +19,12 @@ def valid?
 end
 
 def execute_transaction
-  if 
+  if self.status != "complete" && self.sender.valid? = true
     self.receiver.balance = self.receiver.balance + self.amount
     self.sender.balance = self.sender.balance - self.amount
     self.status = "complete"
   else
-    return "rejected"
+    return "Transaction rejected. Please check your account balance."
   end
 end
 #binding.pry
