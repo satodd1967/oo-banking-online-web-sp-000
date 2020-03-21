3710c7dd03b001d9b3f6d6a284926a58a59cf93c
Seth Todd <seth.todd@outlook.com>
2020-03-21 16:34:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -33,6 +33,9 @@ def execute_transaction
 end
 
 def reverse_transfer
+  self.receiver.balance = self.reciver.balance - self.amount
+  self.sender.balance = self.sender.balance + self.amount
+  self.status = 
 end
 #binding.pry
 
